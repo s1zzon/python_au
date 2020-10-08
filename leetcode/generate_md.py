6745a8f4bbb221b9e44b98e0a6561a1d4151c21a
vladimirdvd <vovadav626@gmail.com>
2020-10-09 01:42:50 +0300

    now it`s more user friendly

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,21 +1,29 @@
+# preparing
+
 old = open(r"C:\Users\User\Desktop\python\source_leetcode_data.txt")
-new = open(r"C:\Users\User\Desktop\python\intervals.md", "a")
-new.write('\n# Intervals\n\n')
+new = open(r"C:\Users\User\Desktop\python\linked-list.md", "a")
 head = old.readline()
 head = head.replace('\n', '')
 lower_head = head.lower()
 split_head = lower_head.split(' ')
+link = old.readline()
+link = link.replace('\n', '')
+full_text = old.read()
+index_def = full_text.find('def')
+body = full_text[index_def-1:]
+
+# writing
+
+new.write('# Linked List\n\n')
 if split_head[2] != '':
     new.write('+[' + head + ']' + '(#' + split_head[0] + '-' + split_head[1] + '-' + split_head[2] + ')\n\n')
 else:
     new.write('+[' + head + ']' + '(#' + split_head[0] + '-' + split_head[1] + ')\n\n')
 new.write('##' + head + '\n\n')
-link = old.readline()
-link = link.replace('\n', '')
 new.write(link + '\n\n')
-full_text = old.read()
-index_def = full_text.find('def')
-body = full_text[index_def-1:-1]
 new.write('```python\n' + body + '\n' + '```')
+
+# closing
+
 old.close()
 new.close()
