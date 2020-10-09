10c7a825e17053d6a4789cdc4bc0d7cd4e6e2921
vladimirdvd <vovadav626@gmail.com>
2020-10-09 13:16:06 +0300

    works w\ any count of words in head

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -8,17 +8,16 @@ lower_head = head.lower()
 split_head = lower_head.split(' ')
 link = old.readline()
 link = link.replace('\n', '')
+index_problems = link.find('problems')
+pointer = link[index_problems + 9:-1]
 full_text = old.read()
 index_def = full_text.find('def')
 body = full_text[index_def-1:]
 
 # writing
 
-new.write('# Linked List\n\n')
-if split_head[2] != '':
-    new.write('+[' + head + ']' + '(#' + split_head[0] + '-' + split_head[1] + '-' + split_head[2] + ')\n\n')
-else:
-    new.write('+[' + head + ']' + '(#' + split_head[0] + '-' + split_head[1] + ')\n\n')
+new.write('\n# Linked List\n\n')
+new.write('+[' + head + ']' + '(#' + pointer + ')\n\n')
 new.write('##' + head + '\n\n')
 new.write(link + '\n\n')
 new.write('```python\n' + body + '\n' + '```')
